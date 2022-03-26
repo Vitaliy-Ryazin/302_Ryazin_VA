d84846f5215f1aad0a35a59426861a6cc023e739
Vitaliy-Ryazin <rmvitaliy@yandex.ru>
2022-03-26 16:59:48 +0300

    Added branch Task04

---
+++ Task04/src/run_test.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace rmvit\task04;
+
+require __DIR__.'/../vendor/autoload.php';
+require 'StackInterface.php';
+require 'Stack.php';
+require 'CompareStrings.php';
+require 'Test.php';
+
+run_test();
