694d8a682e86cb59c75f20495d2f6a94a4a8b9d5
Vitaliy-Ryazin <rmvitaliy@yandex.ru>
2022-03-26 20:23:40 +0300

    Fixed the branch Task04

--- Task04/src/Test.php
+++ Task04/src/Test.php
@@ -4,20 +4,15 @@ namespace rmvit\task04;
 
 function run_test()
 {
-    echo "Ожидание: ". PHP_EOL .
-        "Пример 1: TRUE". PHP_EOL .
-        "Пример 2: TRUE". PHP_EOL .
-        "Пример 3: FALSE". PHP_EOL .
-        "Получено: ". PHP_EOL .
-        "Пример 1: ";
-    if (compareStrings("ab#c", "ade##c")) echo "TRUE". PHP_EOL;
-    else echo"FALSE". PHP_EOL;
-    echo "Пример 2: ";
-    if (compareStrings("a#c", "c")) echo "TRUE". PHP_EOL;
-    else echo"FALSE". PHP_EOL;
-    echo "Пример 3: ";
-    if (compareStrings("abc##", "a#b#")) echo "TRUE". PHP_EOL;
-    else echo"FALSE". PHP_EOL;
+    echo "Ожидание: " . PHP_EOL .
+        "TRUE" . PHP_EOL .
+        "TRUE" . PHP_EOL .
+        "FALSE" . PHP_EOL .
+        "Получено: " . PHP_EOL;
+    echo compareStrings("ab#c", "ade##c") . PHP_EOL;
+    echo compareStrings("a#c", "c") . PHP_EOL;
+    echo compareStrings("abc##", "a#b#") . PHP_EOL;
+    
     $stack = new Stack('a', 'b', 'c', 'd', 'e', 'f', 'g', 'h');
     $stack2 = $stack->copy();
     echo PHP_EOL . "Ожидание: " . PHP_EOL .
@@ -28,10 +23,10 @@ function run_test()
         "[g->f->e->d->c->b->a]" . PHP_EOL .
         "[g->f->e->d->c->b->a] - [h->g->f->e->d->c->b->a]" . PHP_EOL .
         "Получено: " . PHP_EOL;
-    echo $stack->__toString(). PHP_EOL;
-    echo $stack->pop(). PHP_EOL;
-    echo $stack->__toString(). PHP_EOL;
-    echo $stack->top(). PHP_EOL;
-    echo $stack->__toString(). PHP_EOL;
-    echo $stack->__toString(). " - ". $stack2->__toString();
+    echo $stack->__toString() . PHP_EOL;
+    echo $stack->pop() . PHP_EOL;
+    echo $stack->__toString() . PHP_EOL;
+    echo $stack->top() . PHP_EOL;
+    echo $stack->__toString() . PHP_EOL;
+    echo $stack->__toString() . " - " . $stack2->__toString();
 }
